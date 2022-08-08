fcb03d695a49731da81a3361e083a6be09f835d8
rcanfield <Rcanfield86@gmail.com>
2022-08-07 23:15:53 -0400

    Add executor, change loops

--- engine/loops.go
+++ engine/loops.go
@@ -4,34 +4,42 @@ package engine
 // https://stackoverflow.com/a/16466581
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
 
-// ExecuteEachSecondConcurrent Executes a function each second concurrently with a max count
 func ExecuteEachSecondConcurrent(maxSeconds int, delegate func()) {
-	var wg sync.WaitGroup
-	wg.Add(maxSeconds)
-	for _ = range time.Tick(time.Duration(1) * time.Second) {
-		go func() {
+	d := time.NewTicker(1 * time.Second)
+
+	exitChannel := make(chan bool)
+
+	go func() {
+		time.Sleep(time.Duration(maxSeconds) * time.Second)
+		exitChannel <- true
+	}()
+
+	for {
+		select {
+		case <-exitChannel:
+			fmt.Println("Completed!")
+			d.Stop()
+			return
+		case timeElapsed := <-d.C:
+			fmt.Printf("elapsed: %+v\n", timeElapsed)
 			delegate()
-			wg.Done()
-		}()
+		}
 	}
-	wg.Wait()
 }
 
 // ExecuteNumberOfTimesConcurrent Executes a function n number of times with a max concurrent number using a guard
-func ExecuteNumberOfTimesConcurrent(times int, maxConcurrently int, delegate func()) {
-	var guard = make(chan struct{}, maxConcurrently)
+func ExecuteNumberOfTimesConcurrent(times int, delegate func()) {
 	var wg sync.WaitGroup
 	wg.Add(times)
 	for i := 1; i <= times; i++ {
 		go func(n int) {
-			guard <- struct{}{}
 			delegate()
 			wg.Done()
-			<-guard
 		}(i)
 	}
 	wg.Wait()
--- engine/user.go
+++ engine/user.go
@@ -2,8 +2,9 @@ package engine
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"infantry/bindings"
+
+	"github.com/google/uuid"
 )
 
 type User struct {
@@ -13,8 +14,8 @@ type User struct {
 // ExecuteProposal Creates a virtual user and executes the proposal
 func (user User) ExecuteProposal(protocol bindings.Protocol, proposal bindings.Proposal) (bindings.Test, error) {
 	fmt.Printf("protocol: %+v\n", protocol)
-	if protocol.Http != bindings.ProtocolHttp{} {
+	/*if protocol.Http != bindings.ProtocolHttp{} {
 
-	}
+	}*/
 	return bindings.Test{}, nil
 }
